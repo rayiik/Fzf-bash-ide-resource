755677eae6d0084732b1bd44f75818c8ee5a16c5
rayiik <dsimon_aas@hotmail.com>
2020-12-08 09:53:48 +0000

    new file:   script-git/mkgitrep.sh 	new file:   updaterepo.sh
     Untracked files:
    	.bash_completion
            .bash_completion.d/

---
+++ script-git/mkgitrep.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#mk git repo
+echo "# $1" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M master
+git remote add origin https://github.com/rayiik/"$1".git
+git push -u origin master
+
